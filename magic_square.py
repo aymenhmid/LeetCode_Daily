d31b6a707a564a072c74cda80b8661b155d69a1d
Aymen Hmid <48675135+aymenhmid@users.noreply.github.com>
2024-08-11 18:36:10 +0100

    Update magic_square.py

--- magic_square.py
+++ magic_square.py
@@ -46,4 +46,37 @@ class Solution(object):
             if s1 == s2 == s3 == s4 == s5 == s6 == s7 == s8 and all(x == 1  for x in lst) and all(1 <= x <= 9  for x in lst2):
                 count+=1
         return count
-        
+#2nd approach same logic but more clean and optimized code
+class Solution(object):
+    
+    def numMagicSquaresInside(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        ROWS,COLS = len(grid),len(grid[0]
+        def magic(r,c):
+            #check if all square values are in 1-9
+            values = set()
+            for i in range(r,r+3):
+                for j in range(c,c+3):
+                    if grid[i][j] in values or not (1 <= grid[i][j] <= 9):
+                        return 0
+                    values.add(grid[i][j]
+            #check rows sum
+            for i in range(r,r+3):
+                    if sum(grid[i][c:c+3) != 15:
+                               return 0
+            # check cols sum
+            for i in range(c,c+3):
+                    if (grid[r][i] + grid[r+1][i] + grid[r+2][i]) != 15:
+                        return 0
+            #check diagonals sum:
+            if (grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2]) != 15 or  grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c]) != 15 ):
+                    return 0
+            return 1
+    res = 0
+    for r in range(ROWS - 2):
+                for c in range(COLS - 2):
+                    res += magic(r,c)
+    return res
