79a7cf84250ebfe6a696ccf6ace7a5b1fadb2a79
Aymen Hmid <48675135+aymenhmid@users.noreply.github.com>
2025-05-06 10:58:38 +0100

    Create build_array_from_permutation.py

---
+++ Mai_2025/build_array_from_permutation.py
@@ -0,0 +1,17 @@
+#O(n) space
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in  range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
+  #O(1) space
+  class Solution(object):
+    def buildArray(self, nums):
+        for i in range(len(nums)):
+            nums[i] += (1024 * (nums[nums[i]] % 1024))
+        
+        for i in range(len(nums)):
+            nums[i] //= 1024
+        
+        return nums
